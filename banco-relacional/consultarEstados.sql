58a16a0ae287ef8b251505deec052f97d4ff94c2
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-01 14:28:45 -0300

    Update

--- banco-relacional/consultarEstados.sql
+++ banco-relacional/consultarEstados.sql
@@ -1,4 +1,13 @@
 select * from estados
 
-select sigla, nome as 'Nome do Estado' from estados
-where regiao = 'sul'
+select
+      sigla,
+      nome as 'Nome do Estado'
+from estados
+where regiao = 'sul' 
+
+select
+ nome, regiao , populacao 
+from estados
+where populacao >= 10
+order by populacao desc
